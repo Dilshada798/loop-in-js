4b5f535209df243dc9aa9b7b6789ac6553a87dfc
Dilshada798 <82705464+Dilshada798@users.noreply.github.com>
2021-08-26 11:25:32 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,111 @@
+// var a = 1;
+// while(a <= 100){
+//   console.log("Hello World")
+//   a = a + 1
+// }
+
+// var a = 1;
+// while(a <= 100){
+//    console.log(a)
+//    a = a + 1
+// }
+
+// var count = 1;
+// while (count < 10) {
+//    console.log(count);
+//    count +=2;
+// }
+
+// var i=0;
+// while (i<20){
+//     if(i%2==0){
+// console.log(i)
+//     }
+//     i++;
+// }
+ 
+
+// const n = 5;
+
+// looping from i = 1 to 5
+// for (let i = 1; i <= n; i++) {
+//     console.log(`I love JavaScript.`);
+// }
+
+// let sum = 0;
+// const n = 100;
+
+// looping from i = n to 1
+// in each iteration, i is decreased by 1
+// for(let i = n; i >= 1; i-- ) {
+    // adding i to sum in each iteration
+    // sum += i; // sum = sum + i
+
+
+// console.log('sum:',sum);
+
+
+// / take input 3 and find out the ouput by following the code line by line
+// for (var i=0;i<10;) {
+//     console.log(i)
+//  }
+
+// let i=6
+// do{
+//    console.log(i);
+// }
+// while (i >20)
+ 
+ 
+// while(i>20){
+//    console.log(i);
+// }
+
+// const campus_list=["Bangalore","Dharamshala","Pune","Bangalore_another"]
+// for(let campus in campus_list){
+//    console.log(campus)
+
+
+// num=int(input("enter number"))
+// j=1
+// # while num>0:
+// #     j=j*num
+// #     num=num-1
+// #     print("factorial",j)
+
+// }
+
+// function factorialize(num) {
+//     if (num < 0) 
+//           return -1;
+//     else if (num == 0) 
+//         return 1;
+//     else {
+//         return (num * factorialize(num - 1));
+//     }
+//   }
+//   factorialize(5);
+  
+// function factorialize(num) {
+//     // Step 1. Create a variable result to store num
+//     var result = num;
+     
+//     // If num = 0 OR num = 1, the factorial will return 1
+//     if (num === 0 || num === 1) 
+//       return 1; 
+   
+// }  
+// function factorialize(num) {
+//     if (num === 0 || num === 1)
+//       return 1;
+//     for (var i = num - 1; i >= 1; i--) {
+//       num *= i;
+//     }
+//     return num;
+//   }
+//   factorialize(5);
+// let i=2
+// do{
+//     console.log(i);
+// }
+
